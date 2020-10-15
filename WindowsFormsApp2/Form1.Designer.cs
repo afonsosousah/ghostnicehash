d792bc54b92182468f204acf8e0e158347f462df
afonsosousah <afonsosousah@hotmail.com>
2020-10-15 23:28:46 +0100

    uploaded all files

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.start;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(181, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 100);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(57, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hashrate:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wallet";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(82, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "waiting...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(214, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "status";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(180, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 14);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "GPU";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(345, 103);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "DeveloperAddress";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(14, 194);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(440, 45);
+            this.trackBar1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(198, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Intensity";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Max";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(430, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Min";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(466, 300);
+            this.panel1.TabIndex = 14;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(35, 270);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(329, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "GhostNicehash by afonsosousah            Discord: Afonso1234#0128";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(57, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(282, 40);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Create an account on nicehash, and paste here your nicehash wallet address";
+            this.label9.Click += new System.EventHandler(this.label9_Click_1);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(181, 8);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 25);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Options";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(406, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Options";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(57, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Dark Mode";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(345, 131);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(104, 40);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Help me by mining to my address";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(464, 297);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "GhostNicehash";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Threading;
+using System.Drawing.Configuration;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData,
+        UIntPtr dwExtraInfo);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Download Claymore v15
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "claymore.exe") == false)
+            {
+                WebClient Client = new WebClient();
+                Client.DownloadFile("https://cdn.discordapp.com/attachments/754055553470431346/758458942824120350/EthDcrMiner64.exe", AppDomain.CurrentDomain.BaseDirectory + "claymore.exe");
+                Client.DownloadFile("https://cdn.discordapp.com/attachments/754055553470431346/758458933848834058/msvcr110.dll", AppDomain.CurrentDomain.BaseDirectory + "msvcr110.dll");
+                Client.DownloadFile("https://cdn.discordapp.com/attachments/754055553470431346/758458940039102484/libcurl.dll", AppDomain.CurrentDomain.BaseDirectory + "libcurl.dll");
+                Client.DownloadFile("https://cdn.discordapp.com/attachments/754055553470431346/758458937124192266/cudart64_80.dll", AppDomain.CurrentDomain.BaseDirectory + "cudart64_80.dll");
+            }
+
+            label4.ForeColor = Color.Red;
+            label4.Text = "Closed";
+
+            this.FormClosed += Form1_FormClosed;
+
+            ManagementObjectSearcher objvide = new ManagementObjectSearcher("select * from Win32_VideoController");
+
+            foreach (ManagementObject obj in objvide.Get())
+            {
+                label5.Text = ("Detected GPU: " + obj["Name"]);
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Process.GetProcessesByName("claymore").Length > 0 && Process.GetProcessesByName("claymore") != null)
+            {
+                Process proc = Process.GetProcessesByName("claymore")[0];
+                proc.Kill();
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0)
+            {
+                MessageBox.Show("Please add wallet address! (go to options)");
+            }
+            else if(label4.Text=="Mining")
+            {
+                if (Process.GetProcessesByName("claymore").Length > 0 && Process.GetProcessesByName("claymore") != null)
+                {
+                    Process proc = Process.GetProcessesByName("claymore")[0];
+                    proc.Kill();
+                }
+            }
+            else
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.CreateNoWindow = true;
+                startInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + "claymore.exe";
+                startInfo.Arguments = "-epool daggerhashimoto.eu.nicehash.com:3353 -ewal " + textBox1.Text.Trim() + " -eworker afhMiner -li " + trackBar1.Value + " -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -erate 1 -allcoins 1 -allpools 1";
+                process.StartInfo = startInfo;
+                process.Start();
+                SetParent(process.MainWindowHandle, this.Handle);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Process.GetProcessesByName("claymore").Length > 0 && Process.GetProcessesByName("claymore") != null)
+            {
+                
+                using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    try
+                    {
+                        var receiveBytes = new byte[256];
+                        socket.Connect("localhost", 3333);
+                        socket.Send(Encoding.ASCII.GetBytes("{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"miner_getstat1\"}"));
+                        socket.Receive(receiveBytes, receiveBytes.Length, SocketFlags.None);
+                        var responseJson = Encoding.ASCII.GetString(receiveBytes);
+                        if (responseJson.Split(',')[4].Trim(new char[] { ' ', '"' }) != "" && responseJson.Split(',')[4].Trim(new char[] { ' ', '"' }) != " ")
+                        {
+                            int hashrate = int.Parse(responseJson.Split(',')[5].Trim(new char[] { ' ', '"' }));
+                            label3.Text = String.Format("{0,0:N3}", hashrate / 1000.0) + "MH/s";
+                        }
+                        if (responseJson.Split(',')[5].Trim(new char[] { ' ', '"' }) != "" && responseJson.Split(',')[5].Trim(new char[] { ' ', '"' }) != " ")
+                        {
+                            int hashrate = int.Parse(responseJson.Split(',')[5].Trim(new char[] { ' ', '"' }));
+                            label3.Text = String.Format("{0,0:N3}", hashrate / 1000.0) + "MH/s";
+                        }   
+                    }
+                    catch (Exception)
+                    {
+                        
+                    }
+                        
+
+                    label4.ForeColor = Color.Green;
+                    label4.Text = "Mining";
+
+                    button1.BackgroundImage = WindowsFormsApp2.Properties.Resources.mining;
+                }
+
+            }
+            else
+            {
+                label4.ForeColor = Color.Red; 
+                label4.Text = "Closed";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "3F1zYbiCB5JKR21GiFgFwzxZoyJzTsk3U2";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(panel1.Visible==false)
+            {
+                panel1.Visible = true;
+            }
+            else if(panel1.Visible==true)
+            {
+                panel1.Visible = false;
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label9_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(this.BackColor==SystemColors.ButtonFace)
+            {
+                this.BackColor = SystemColors.ControlDarkDark;
+                button4.Text = "Light Mode";
+            }
+            else if(this.BackColor == SystemColors.ControlDarkDark)
+            {
+                this.BackColor = SystemColors.ButtonFace;
+                button4.Text = "Dark Mode";
+            }
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
